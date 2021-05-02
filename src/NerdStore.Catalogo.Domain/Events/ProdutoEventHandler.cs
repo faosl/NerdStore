e46a3a7f12e73be89b40db8c6476b64a52dcfeb6
Fellipe Augusto <faosl@hotmail.com>
2021-05-02 11:34:05 -0300

    Lançando eventos de domínio.

---
+++ src/NerdStore.Catalogo.Domain/Events/ProdutoEventHandler.cs
@@ -0,0 +1,28 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace NerdStore.Catalogo.Domain.Events
+{
+    public class ProdutoEventHandler : INotificationHandler<ProdutoAbaixoEstoqueEvent>
+    {
+
+        private readonly IProdutoRepository _produtoRepository;
+
+        public ProdutoEventHandler(IProdutoRepository produtoRepository)
+        {
+            _produtoRepository = produtoRepository;
+        }
+
+        public async Task Handle(ProdutoAbaixoEstoqueEvent notification, CancellationToken cancellationToken)
+        {
+            var produto = await _produtoRepository.ObterPorId(notification.AggregateId);
+
+            //Enviar um e-mail para aquisição de mais produtos.
+            
+        }
+    }
+}
